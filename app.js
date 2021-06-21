be4bea5d46d3a5b88c9143c909cd3f7c41f074ef
Patrick Tsang <scenage@gmail.com>
2021-06-21 10:57:11 +1000

    Refactored and desconstructed objects.

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const request = require('postman-request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const { error } = require('console')
 
-geocode ('Castle Hill, Sydney', (error, data) => {
-    console.log(error)
-    console.log(data)
+function getAddress () {
+    const address = process.argv[2]
+    if (address === undefined){
+        console.log('No address provided!')
+        return undefined
+    }
+    return address
+}
+
+const address = getAddress()
+
+geocode (address, (error, { latitude, longitude, location} = {}) => {
+
+    if (error || address === undefined) {
+        return console.log(error)
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(location)
+        console.log('Data', forecastData)
+      })
 })
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-//-75.7088, 44.1545
-forecast(-33.72925, 151.00402, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,17 +2,16 @@ const request = require('postman-request')
 
 const forecast =  (lat, long, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=0039a8b003fbedc35ef0c3bb2c0fd837&query=' + lat + ',' + long + '&units=f'
-    console.log(url)
-    request({ url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body } = {}) => {
         if (error) {
             callback ('Unable to connect to the location services!', undefined)
-        } else if (response.body.success === false) {
+        } else if (body.success === false) {
             callback ('Unable to find location. Try another search!', undefined)
         } else {
             callback (undefined, {
-                temperature: response.body.current.temperature,
-                weather: response.body.current.weather_descriptions,
-                location: response.body.location.name
+                temperature: body.current.temperature,
+                weather: body.current.weather_descriptions,
+                location: body.location.name
             })
         }
     })
